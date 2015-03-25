4de7d9ae468381ed18a52a40b5095f98f5dda8af
Jesse Monroy <jessemonroy650@yahoo.com>
2015-03-25 15:34:46 -0700

    final update

--- gpsNotepad.js
+++ gpsNotepad.js
@@ -26,7 +26,7 @@ var Location = {
 	},
 
 	watchLocation : function () {
-		alert('watch');
+		//alert('watch');
 		this.watchID = navigator.geolocation.watchPosition(this.callback.onSuccess, this.callback.onError, this.geoLocationOption);
 	},
 
